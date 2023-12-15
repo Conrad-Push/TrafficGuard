865b1607a8557104f46c213b107622492b849721
pploszczyca <piotrus.plo@gmail.com>
2023-12-15 16:19:40 +0100

    Moved printing statistics to separate method

--- models_training/classifier.py
+++ models_training/classifier.py
@@ -1,11 +1,27 @@
 from abc import ABC, abstractmethod
 
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+
 
 class Classifier(ABC):
     def __init__(self, training_data, test_data):
         self.training_data = training_data
         self.test_data = test_data
+        self.model = None
 
     @abstractmethod
     def train_and_evaluate(self):
         pass
+
+    def print_statistics(self):
+        X_test = self.test_data.drop('Class', axis=1)
+        y_test = self.test_data['Class']
+
+        predictions = self.model.predict(X_test)
+        accuracy = accuracy_score(y_test, predictions)
+        confusion = confusion_matrix(y_test, predictions)
+        report = classification_report(y_test, predictions)
+
+        print(f"Accuracy: {accuracy}")
+        print(f"Confusion:\n{confusion}")
+        print(f"Report:\n{report}")
--- models_training/main.py
+++ models_training/main.py
@@ -4,17 +4,7 @@ from models_training.k_neighbors_classifier import KNeighborsClassifierModel
 
 
 def main(training_data, test_data):
-    # Naive Bayes Classifier
-    print("Naive Bayes Classifier:\n")
-    nb_classifier = NaiveBayesClassifier(training_data, test_data)
-    nb_classifier.train_and_evaluate()
-
-    # Decision Tree Classifier
-    print("\nDecision Tree Classifier:\n")
-    dt_classifier = DecisionTreeClassifierModel(training_data, test_data)
-    dt_classifier.train_and_evaluate()
-
-    # K Neighbors Classifier
-    print("\nK Neighbors Classifier:\n")
-    kn_classifier = KNeighborsClassifierModel(training_data, test_data)
-    kn_classifier.train_and_evaluate()
+    for classifier in [NaiveBayesClassifier, DecisionTreeClassifierModel, KNeighborsClassifierModel]:
+        model = classifier(training_data, test_data)
+        model.train_and_evaluate()
+        model.print_statistics()
